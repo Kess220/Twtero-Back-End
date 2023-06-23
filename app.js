705ababb5eaececef9b9bab96bbc7901a4996358
unknown <prodvictorkaio@gmail.com>
2023-06-23 11:18:49 -0300

    concertando algumas coisas do bonus

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const usuarios = [];
 const tweets = [];
